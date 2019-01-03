d56770f648747b2aaa6262eb18ae8ca4b216fa7c
xingfa <zhxf2005307@163.com>
2019-01-03 18:04:22 +0800

    Initial Commit

---
+++ MyMoji/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyMoji
+//
+//  Created by Xingfa Zhou on 2019/1/3.
+//  Copyright © 2019 BeterLife. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

3d8825340169e65d0cd297d9b8a0de22ff1060f6
xingfa <zhxf2005307@163.com>
2019-01-24 19:50:29 +0800

    添加自动截图的相关配置

--- MyMojiUITests/MyMojiUITests.m
+++ MyMojiUITests/MyMojiUITests.m
@@ -2,11 +2,13 @@
 //  MyMojiUITests.m
 //  MyMojiUITests
 //
-//  Created by Xingfa Zhou on 2019/1/3.
+//  Created by Xingfa Zhou on 2019/1/24.
 //  Copyright © 2019 BeterLife. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
+
+#import "MyMojiUITests-Swift.h"
 
 @interface MyMojiUITests : XCTestCase
 
@@ -21,8 +23,11 @@
     self.continueAfterFailure = NO;
 
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
 
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    [Snapshot setupSnapshot:app];
+    [app launch];
+    
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
 
@@ -33,6 +38,17 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    
+    XCUIElementQuery *tabBarsQuery = [[XCUIApplication alloc] init].tabBars;
+    [tabBarsQuery.buttons[@"Kaomoji"] tap];
+    [Snapshot snapshot:@"Kaomoji" timeWaitingForIdle:10];
+    [tabBarsQuery.buttons[@"Favorites"] tap];
+    [Snapshot snapshot:@"Favorites" timeWaitingForIdle:10];
+    [tabBarsQuery.buttons[@"Settings"] tap];
+    [Snapshot snapshot:@"Settings" timeWaitingForIdle:10];
+   
+
 }
 
 @end
